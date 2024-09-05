19f1d621480cc6d7d65b842ef72f0a4e62a8ae22
Mohanaprasath5 <mohanmp5555@gmail.com>
2024-09-05 12:52:51 +0530

    Updated index.html, new.js, and styles.css for layout improvements

--- new.js
+++ new.js
@@ -1,26 +1,16 @@
-// let product=(a,b)=>a*b;
-// console.log(product(2,3));
-
-// let add=(a,b)=>a+b;
-// console.log(add(5,2));
-
-// let arr=[2,3];
-// let sum=arr=>{
-//     let num=0;
-//     for(let i of arr ){
-//         num+=i;
-//     }
-//     return num;
-// }
-
-// console.log(sum(arr));
-
-let button = document.getElementById("button");
-button.addEventListener("click", greet);
-function greet() {
-  let name = document.getElementById("input").value;
-  console.log(name);
-  // let output=`hii ${name}`;
-  let result = document.getElementById("result");
-  result.innerHTML = `hii ${name}`;
-}
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+};
